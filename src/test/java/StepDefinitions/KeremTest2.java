489d1b169a0644181fdb31374e1d9bc7898585be
Kerem <keremdizdar@gmail.com>
2023-09-30 17:42:33 +0300

    test12344

--- src/test/java/StepDefinitions/KeremTest2.java
+++ src/test/java/StepDefinitions/KeremTest2.java
@@ -3,5 +3,6 @@ package StepDefinitions;
 public class KeremTest2 {
     public static void main(String[] args) {
         System.out.println("Salimcik");
+        System.out.println("Orhancık");
     }
 }
