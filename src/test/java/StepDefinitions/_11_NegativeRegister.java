975ea9bf0cb8e3de2d6706ad6ce0b9693e1b3d23
Serkan <ozsahinserkan@gmail.com>
2023-10-01 16:50:36 +0300

    negativeregister1, negativeregister2 and forgotlogininfo done.

---
+++ src/test/java/StepDefinitions/_11_NegativeRegister.java
@@ -0,0 +1,21 @@
+package StepDefinitions;
+
+import Pages.SODialogContent;
+import io.cucumber.java.en.Then;
+
+public class _11_NegativeRegister {
+
+    SODialogContent dc = new SODialogContent();
+
+    @Then("Username already exist message is displayed")
+    public void usernameAlreadyExistMessageIsDisplayed() {
+
+        dc.verifyContainsText(dc.existsMessage, "exists");
+    }
+
+    @Then("Password did not match message is displayed")
+    public void passwordDidNotMatchMessageIsDisplayed() {
+
+        dc.verifyContainsText(dc.passwordMatchMessage, " not match");
+    }
+}
